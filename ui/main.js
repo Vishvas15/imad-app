ece648ab18fe11952e6a0f2d87e322bf3dd65a87
Vishvas15 <36596750+Vishvas15@users.noreply.github.com>
2018-03-06 21:03:24 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,7 +1,13 @@
 console.log('Loaded!');
 var img=document.getElamentById('madi');
+var marginLeft=0;
+function moveRight()
+{
+    marginLeft=marginLeft+10;
+    img.style.marginLeft=marginLeft+'px';
+}
 img.onclick=function()
 {
     var interval=setInterval(moveRight,100);
     //img.style.marginLeft='100px';
-}
+};
