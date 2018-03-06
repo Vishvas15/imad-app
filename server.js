c79db412f6b65c6fc1b91069f1ab8a114c7a0974
Vishvas15 <36596750+Vishvas15@users.noreply.github.com>
2018-03-06 20:37:19 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,6 +9,17 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
+app,get('/artical-one',function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'artical-one.html'));
+});
+
+app,get('/artical-two',function (req, res) {
+  res.send('artical 2 requested');
+});
+
+app,get('/artical-three',function (req, res) {
+  res.send('artical 3 requested');
+});
 
 app.get('/ui/style.css',function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
