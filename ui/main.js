e0f1382667b188acc3f58d72742b4d8fd3cf71fb
Vishvas15 <36596750+Vishvas15@users.noreply.github.com>
2018-03-07 21:53:27 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -75,6 +75,6 @@ submit.onclick = function(){
     //make the request
     var nameInput = document.getElementById('name');
     var name=nameInput.value;
-    request.open('GET','http://vishwasmangroliya15.imad.hasura-app.io/counter',true);
+    request.open('GET','http://vishwasmangroliya15.imad.hasura-app.io/submit-name?name=' +name,true);
     request.send(null);
 };
